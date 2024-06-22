3df82cae7fcf85ba6e6e60ef714deb87b4b0aec6
Andrei <andreifrancap@gmail.com>
2024-06-22 19:11:02 -0300

    add: rota pg de venda e realizar venda

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.status(200).render("home");
 });
 
@@ -35,9 +35,57 @@ app.get("/corretor", (req,res) => {
     
 });
 
-app.get("/anunciar-imovel", (req,res) => {
-    res.status(200).render("propriedade");
+app.get("/realizar-venda", (req, res) => {
+    connecta.query("SELECT * FROM clientes;", (err, clientesResults) => {
+        if (err) {
+            console.error("Erro na consulta: " + err);
+            res.status(500).render("erro");
+            return;
+        }
+
+        connecta.query("SELECT * FROM corretor;", (err, corretoresResults) => {
+            if (err) {
+                console.error("Erro na consulta: " + err);
+                res.status(500).render("erro");
+                return;
+            }
+
+            connecta.query(`
+                SELECT Propriedade.*, clientes.nome, clientes.sobrenome
+                FROM Propriedade
+                JOIN clientes ON Propriedade.cliente_ID = clientes.id;
+            `, (err, propriedadesResults) => {
+                if (err) {
+                    console.error("Erro na consulta: " + err);
+                    res.status(500).render("erro");
+                    return;
+                }
+
+                res.status(200).render("venda", {
+                    clientes: clientesResults,
+                    corretores: corretoresResults,
+                    propriedades: propriedadesResults
+                });
+            });
+        });
+    });
 });
+
+
+app.post("/venda", (req, res) => {
+    let {propriedade_id, cliente_id, corretor_id, valor, forma_pagamento, qtd_parcelas} = req.body;
+    let venda = {propriedade_id, cliente_id, corretor_id, valor, forma_pagamento, qtd_parcelas};
+    let query = connecta.query("INSERT INTO venda SET ?", venda, (err) => {
+        if(err){
+            console.error("Erro ao inserir na tabela vendas " + err);
+            res.status(500).send("Erro");
+            return;
+        }
+        console.log("Venda inserida no banco de dados");
+    });
+    res.status(202).redirect("/realizar-venda");
+});
+
 
 app.post('/cadastroCli', (req,res) => {
     const {nome,sobrenome,email,endereco,cidade, estado,cep} = req.body;
