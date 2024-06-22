90ba353631a2bbc78234432473d3b3057d85fe42
GUTOMADON <gutomadon@gmail.com>
2024-06-22 18:32:05 -0300

    add: table Propriedade

--- banco.sql
+++ banco.sql
@@ -1,3 +1,4 @@
+-- Active: 1719089674359@@localhost@3306@banco
 CREATE DATABASE IF NOT EXISTS banco;
 
 USE banco;
@@ -11,4 +12,20 @@ CREATE TABLE clientes(
     cidade VARCHAR(255) NOT NULL,
     estado VARCHAR(255) NOT NULL,
     cep VARCHAR(255) NOT NULL
+);
+CREATE TABLE Propriedade (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Valor FLOAT,
+    Cliente_ID VARCHAR(255),
+    Corretor_Id VARCHAR(255),
+    Rua VARCHAR(255),
+    Numero INT,
+    Bairro VARCHAR(255),
+    Cidade VARCHAR(255),
+    Estado VARCHAR(255),
+    Pais VARCHAR(255),
+    CEP VARCHAR(255),
+    Tipo VARCHAR(255),
+    Area_m2 FLOAT,
+    Disponibilidade VARCHAR(255)
 );
