34465db5acd78714e3c40d2172b574733c9bf16f
KaiquePerciliano <kaiqueperciliano374@gmail.com>
2024-06-24 01:30:08 -0300

    add: route to update and delete corretor

--- index.js
+++ index.js
@@ -86,6 +86,21 @@ app.listen(3000, () => {
 
 
 
+app.get('/corretor', (req, res) => {
+    connecta.query("SELECT * FROM corretor;", (err, results, fields) => {
+        if(err){
+            console.error("Erro na cosulta" + err);
+            res.status(500).render("erro");
+            return;
+        }
+        res.status(200).render("corretor", {corretores: results});
+    });
+});
+
+app.get("/corretor/cadastraCorretor", (req,res) => {
+    res.status(200).render("cadastraCorretor"); 
+});
+
 
 app.post('/corretor/cadastroCorretor', (req,res) => {
     let {nome,email, cpf, telefone, localidade} = req.body;
@@ -99,4 +114,58 @@ app.post('/corretor/cadastroCorretor', (req,res) => {
     })
     console.log("Corretor inserido no banco de dados");
     res.status(202).redirect("/corretor");
+});
+
+
+app.delete("/corretor/:id", (req, res) => {
+    const corretorId = req.params.id;
+    const query = connecta.query("DELETE FROM corretor WHERE id = ?", [corretorId], (err, result) => {
+        if (err) {
+            console.error("Erro ao excluir corretor " + err);
+            res.status(500).send("Erro");
+            return;
+        }
+        if (result.affectedRows === 0) {
+            res.status(404).send("Corretor não encontrado!");
+            return;
+        }
+        console.log("Corretor excluído");
+        res.redirect("/corretor");
+    });
+});
+
+app.get("/editarCorretor/:id", (req, res) => {
+    const corretorId = req.params.id;
+    connecta.query("SELECT * FROM corretor WHERE id = ?", [corretorId], (err, results) => {
+        if (err) {
+            console.error("Erro na consulta: " + err);
+            res.status(500).render("erro");
+            return;
+        }
+        if (results.length === 0) {
+            res.status(404).render("erro");
+            return;
+        }
+        res.status(200).render("editarCorretor", { corretor: results[0] });
+    });
+});
+
+app.put("/corretor/:id", (req, res) => {
+    const corretorId = req.params.id;
+    let {nome,email, telefone, localidade} = req.body;
+    let corretor = {nome,email, telefone, localidade};
+
+    const query = connecta.query("UPDATE corretor SET ? WHERE id = ?", [corretor, corretorId], (err, result) => {
+        if (err) {
+            console.error("Erro ao atualizar a tabela corretor: " + err);
+            res.status(500).send("Erro");
+            return;
+        }
+        if (result.affectedRows === 0) {
+            res.status(404).send("Corretor não encontrado!");
+            return;
+        }
+        console.log("Corretor atualizado");
+        res.status(200).send("Corretor atualizado");
+    });
 });
